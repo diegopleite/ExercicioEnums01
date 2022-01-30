e66877b4f2f617f9a5decb588f55689d5c830105
Diego Leite <diego@DESKTOP-ME841EH>
2022-01-30 14:37:51 -0300

    Criacao classe Department

---
+++ ExercicioWorker_ENUMS/Department.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioWorker_ENUMS {
+    class Department {
+        public string Name { get; private set; }
+    }
+}
