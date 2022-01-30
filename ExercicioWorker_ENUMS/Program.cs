c66d25bc33343b495e774814a75cea49e93e913e
Diego Leite <diego@DESKTOP-ME841EH>
2022-01-30 14:15:40 -0300

    Iniciando exercicio

---
+++ ExercicioWorker_ENUMS/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ExercicioWorker_ENUMS {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
