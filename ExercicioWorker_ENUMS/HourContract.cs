b8b6f0ee8f46de20e7e4ccfd927e109e5ce2a08d
Diego Leite <diego@DESKTOP-ME841EH>
2022-01-30 14:36:49 -0300

    atualizacao classe Worker

--- ExercicioWorker_ENUMS/HourContract.cs
+++ ExercicioWorker_ENUMS/HourContract.cs
@@ -6,7 +6,7 @@ namespace ExercicioWorker_ENUMS {
     class HourContract {
         public DateTime Date { get; set; }
         public double ValuePerHour { get; set; }
-        public int Hours { get; set; }
+        public int Hours { get; set; }        
 
         public HourContract(DateTime date, double valuePerHour, int hours) {
             Date = date;
--- ExercicioWorker_ENUMS/Worker.cs
+++ ExercicioWorker_ENUMS/Worker.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ExercicioWorker_ENUMS.Entities;
 
 namespace ExercicioWorker_ENUMS {
     class Worker {
         public string Name { get; set; }
-        public WorkerLeve Level { get; private set; }
-        public double BaseSalary { get; private set }
+        public WorkLevel Level { get; private set; }
+        public double BaseSalary { get; private set; }
 
-        public Worker(string name, WorkerLeve level, double baseSalary) {
+        public Worker(string name, WorkLevel level, double baseSalary) {
             Name = name;
             Level = level;
             BaseSalary = baseSalary;
