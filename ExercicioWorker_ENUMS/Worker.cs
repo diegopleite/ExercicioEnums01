b8432ddcf10a2d819e3391d94755d71551c5e735
Diego Leite <diego@DESKTOP-ME841EH>
2022-01-30 14:23:36 -0300

    Criacao classe Worker

---
+++ ExercicioWorker_ENUMS/Worker.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioWorker_ENUMS {
+    class Worker {
+        public string Name { get; set; }
+        public WorkerLeve Level { get; private set; }
+        public double BaseSalary { get; private set }
+
+        public Worker(string name, WorkerLeve level, double baseSalary) {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+        }
+
+        public void addContract(HourContract contract) {
+
+        }
+
+        public void removeContract(HourContract contract) {
+
+        }
+
+        public double income(int year, int month) {
+
+        }
+
+        public override string ToString() {
+            return Name
+                + (", ")
+                + Level
+                + (", ")
+                + BaseSalary;
+        }
+
+    }
+}
