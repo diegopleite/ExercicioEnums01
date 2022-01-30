996e69e743fc2f5d5b127946152be5f86ea1eafe
Diego Leite <diego@DESKTOP-ME841EH>
2022-01-30 14:28:32 -0300

    Criacao classe WorkLevel

---
+++ ExercicioWorker_ENUMS/Entities/WorkLevel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioWorker_ENUMS.Entities {
+    enum WorkLevel : int {
+
+        JUNIOR = 0,
+        MID_LEVEL = 1,
+        SENIOR = 2        
+
+    }
+}
