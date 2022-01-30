958fa990042121b2d114ac121bb0c9a89d033df9
Diego Leite <diego@DESKTOP-ME841EH>
2022-01-30 14:33:11 -0300

    Criacao classe HourContract

---
+++ ExercicioWorker_ENUMS/HourContract.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioWorker_ENUMS {
+    class HourContract {
+        public DateTime Date { get; set; }
+        public double ValuePerHour { get; set; }
+        public int Hours { get; set; }
+
+        public HourContract(DateTime date, double valuePerHour, int hours) {
+            Date = date;
+            ValuePerHour = valuePerHour;
+            Hours = hours;
+        }
+
+        public double totalValue() {
+            return ValuePerHour * Hours;
+        }
+    }
+}
